dffc7a00ffbc4f592c2f390e48f2fca41f146ff3
Andeviking <422563809@qq.com>
2022-12-20 11:55:28 +0800

    The code of project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    //int id = QFontDatabase::addApplicationFont(":/fonts/fonts/corbel.ttf");
+    //qDebug() << id;
+    //QFontDatabase::addApplicationFont(":/fonts/fonts/corbell.ttf");
+    //Qt::AA_EnableHighDpiScaling;
+#ifdef Q_OS_LINUX
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QApplication a(argc, argv);
+
+    //creat initial graph
+    MainWindow w;
+    w.setWindowFlag(Qt::FramelessWindowHint);
+    w.setAttribute(Qt::WA_TranslucentBackground);
+    w.show();
+    return a.exec();
+
+}
